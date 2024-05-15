04586e3f2a9b5a5de7f715a2586aa2727f8be746
Klaudia Wolska <klaudia@macbook-air-klaudia.home>
2024-05-15 17:35:58 +0200

    bootrstrap welcome page

--- work-tracker/resources/views/home.blade.php
+++ work-tracker/resources/views/home.blade.php
@@ -5,12 +5,64 @@
     .dashboard-container {
         justify-content: space-between;
         border-radius: 10px;
+        margin-top: 20px;
+        margin-bottom: 20px;
+    }
+    .bg-1{
+        background-color: #f1f3f5;
+    }
+    .welcome-box{
+        height: 30px;
+    }
+    .vertical-center {
+            min-height: 15px;
+            display: flex;
+            align-items: center;
+    }
+    .header-box{
+        background-color: #16C7AA;
+        border-radius: 10px;
+        text-align: center;
+        font-weight: bold;
+        width: 100%;
+    }
+    .dashboard-links {
+        display: flex;
+        justify-content: space-between;
+        padding: 5px;
+        text-align: center;
     }
 
-    .bg-1{
-        /* background-color: #f1f3f5; */
-        background-color: red;
+    .dashboard-links a {
+        color: black;
+        text-decoration: none; 
+        font-weight: bold;
+        background-color: #C3C3C3;
+        border-radius: 10px;
+        font-size: small;
+        flex-grow: 1;
     }
+    .box-1{
+        height: auto;
+    }
+    .akcept-box{
+        background-color: #16C7AA;
+        border-radius: 10px;
+        justify-content: center;
+        font-weight: bold;
+        height: 50px;
+    }
+    .box-2{
+        height: 400px;
+    }
+    .box-3, .box-4{
+        height: auto;
+    }
+    .urlopy-box{
+        height: 80px;
+    }
+
+
 </style>
 @endsection
 
@@ -21,7 +73,7 @@
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body car-box">
+                <!-- <div class="card-body car-box">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
@@ -39,48 +91,117 @@
                     @endif
 
                     {{ __('You are logged in!') }}
-                </div>
+                </div> -->
     
-                @if (Auth::user()->role == 2 || Auth::user()->role == 3)
-                <div class="row">
+                
+                <div class="row p-3">
                     <div class="col-md-4">
-                            <div class="dashboard-container bg-1 px-5">
-                                <div class="dashboard-links">
-                                    <a href="{{ route('employees.index') }}">Przejdź do Listy Pracowników</a>
-                                </div>
-                                <div class="dashboard-links">
-                                    <a href="{{ route('accounts.index') }}">Zarządzanie kontami</a>
-                                </div>
-                            </div>
-                    </div>
-                    <div class="col-md-4">
-                            <div class="dashboard-container bg-1 px-5">
-                                <div class="dashboard-links">
-                                        <a href="{{ route('employments.index') }}">Zatrudnienia</a>
+                            <div class="dashboard-container welcome-box bg-1 px-5 vertical-center">
+                                @if(Auth::check())
+                                    <div>
+                                        Witaj, <strong>{{ Auth::user()->first_name }}</strong>!
                                     </div>
-                                <div class="dashboard-links">
-                                    <a href="{{ route('vacations.adminIndex') }}">Zarządzaj urlopami</a>
+                                @endif     
+                            </div>
+                            @if (Auth::user()->role == 2 || Auth::user()->role == 3)
+                            <div class="dashboard-container box-1 bg-1">
+                                <div class="header-box">
+                                    Pracownicy
+                                </div>
+
+                                <div class="p-3">
+                                    <div class="row mt-3">
+                                        <div class="col-md-7">
+                                            <div style="text-align: center;">Pracownicy do zaakceptowania</div>
+                                            <div class="akcept-box vertical-center">
+                                                X
+                                            </div>
+                                            
+
+                                        </div>
+                                        <div class="col-md-5">
+                                            <div class="dashboard-links vertical-center">
+                                                <a href="{{ route('employees.index') }}">Lista pracowników</a>
+                                            </div>
+                                            <div class="dashboard-links vertical-center">
+                                                <a href="{{ route('accounts.index') }}">Zarządzanie kontami</a>
+                                            </div>
+                                            <div class="dashboard-links vertical-center">
+                                                <a href="{{ route('employments.index') }}">Zatrudnienia</a>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                     </div>
-
-                    <!-- <a href="{{ route('employees.index') }}">Przejdź do Listy Pracowników</a>
-                    <a href="{{ route('accounts.index') }}">Zarządzanie kontami</a>
-                    <a href="{{ route('employments.index') }}">Zatrudnienia</a>
-                    <a href="{{ route('vacations.adminIndex') }}">Zarządzaj urlopami</a> -->
-                @endif
-                <div class="col-md-4">
-                        <div class="dashboard-container bg-1 px-5">
-                            <div class="dashboard-links">
-                                <a href="{{ route('employees.my-months') }}">Zobacz swoją pracę</a>
+                    <div class="col-md-4">
+                        <div class="dashboard-container box-2 bg-1">
+                            <div class="header-box">
+                                Wiadomości
                             </div>
-                            <div class="dashboard-links">
-                                <a href="{{ route('vacations.index') }}">Urlopy</a>
+
+                            <div class="m-3">
+                                X
                             </div>
                         </div>
+                    </div>
+                    @endif
+                    
+                    <div class="col-md-4">
+                    @if (Auth::user()->role == 2 || Auth::user()->role == 3)
+                        <div class="dashboard-container box-3 bg-1">
+                            <div class="header-box">
+                                Urlopy
+                            </div>
+
+                            <div class="p-2">
+                                    <div class="dashboard-container bg-1 px-5">
+                                        <div class="dashboard-links">
+                                            <a href="{{ route('vacations.adminIndex') }}">Zarządzaj urlopami</a>
+                                        </div>
+                                        <div class="row mt-1">
+                                            <div class="col-md-6">
+                                                <div style="text-align: center;">Nowe prośby</div>
+                                                <div class="akcept-box vertical-center">
+                                                    X
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div style="text-align: center;">Wszystkie prośby</div>
+                                                <div class="akcept-box vertical-center">
+                                                    X
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                
+                            </div>
+                        </div>
+                    @endif  
+                        <div class="dashboard-container box-4 bg-1">
+                            <div class="header-box">
+                                Moje dane
+                            </div>
+                            <div class="p-3">
+                            <div class="dashboard-links mt-3">
+                                <a href="{{ route('employees.my-months') }}">Moja praca</a>
+                            </div>
+                                <div class="row mt-3">
+                                            <div class="col-md-7">
+                                                <div style="text-align: center;">Nadchodzące urlopy</div>
+                                                <div class="akcept-box vertical-center">
+                                                    X
+                                                </div>
+                                                
+                                            </div>
+                                            <div class="col-md-5">
+                                                <div class="dashboard-links urlopy-box">
+                                                    <a href="{{ route('vacations.index') }}" class="vertical-center" style="justify-content:center;">Moje urlopy</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                            </div>
                 </div>
-                <!-- <a href="{{ route('employees.my-months') }}">Zobacz swoją pracę</a>
-                <a href="{{route('vacations.index') }}">Urlopy</a>  -->
             </div>
         </div>
     </div>
