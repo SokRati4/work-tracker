96fbb66fb2915c2360c5331958b60a700bc5ff42
Klaudia Wolska <klaudia@macbook-air-klaudia.home>
2024-05-15 07:17:23 +0200

    m

--- work-tracker/resources/views/home.blade.php
+++ work-tracker/resources/views/home.blade.php
@@ -4,18 +4,11 @@
 <style>
     .dashboard-container {
         justify-content: space-between;
-        margin-bottom: 20px;
         border-radius: 10px;
     }
 
-    .dashboard-links {
-        padding: 10px;
-    }
     .bg-1{
         background-color: #f1f3f5;
-    }
-    .container{
-        padding: 10px;
     }
 </style>
 @endsection
@@ -27,7 +20,7 @@
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
+                <div class="card-body car-box">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
@@ -50,24 +43,24 @@
                 @if (Auth::user()->role == 2 || Auth::user()->role == 3)
                 <div class="row">
                     <div class="col-md-4">
-                        <div class="dashboard-container bg-1">
-                            <div class="dashboard-links">
-                                <a href="{{ route('employees.index') }}">Przejdź do Listy Pracowników</a>
+                            <div class="dashboard-container bg-1 px-5">
+                                <div class="dashboard-links">
+                                    <a href="{{ route('employees.index') }}">Przejdź do Listy Pracowników</a>
+                                </div>
+                                <div class="dashboard-links">
+                                    <a href="{{ route('accounts.index') }}">Zarządzanie kontami</a>
+                                </div>
                             </div>
-                            <div class="dashboard-links">
-                                <a href="{{ route('accounts.index') }}">Zarządzanie kontami</a>
-                            </div>
-                        </div>
                     </div>
                     <div class="col-md-4">
-                        <div class="dashboard-container bg-1">
-                            <div class="dashboard-links">
-                                    <a href="{{ route('employments.index') }}">Zatrudnienia</a>
+                            <div class="dashboard-container bg-1 px-5">
+                                <div class="dashboard-links">
+                                        <a href="{{ route('employments.index') }}">Zatrudnienia</a>
+                                    </div>
+                                <div class="dashboard-links">
+                                    <a href="{{ route('vacations.adminIndex') }}">Zarządzaj urlopami</a>
                                 </div>
-                            <div class="dashboard-links">
-                                <a href="{{ route('vacations.adminIndex') }}">Zarządzaj urlopami</a>
                             </div>
-                        </div>
                     </div>
 
                     <!-- <a href="{{ route('employees.index') }}">Przejdź do Listy Pracowników</a>
@@ -76,14 +69,14 @@
                     <a href="{{ route('vacations.adminIndex') }}">Zarządzaj urlopami</a> -->
                 @endif
                 <div class="col-md-4">
-                    <div class="dashboard-container bg-1">
-                        <div class="dashboard-links">
-                            <a href="{{ route('employees.my-months') }}">Zobacz swoją pracę</a>
+                        <div class="dashboard-container bg-1 px-5">
+                            <div class="dashboard-links">
+                                <a href="{{ route('employees.my-months') }}">Zobacz swoją pracę</a>
+                            </div>
+                            <div class="dashboard-links">
+                                <a href="{{ route('vacations.index') }}">Urlopy</a>
+                            </div>
                         </div>
-                        <div class="dashboard-links">
-                            <a href="{{ route('vacations.index') }}">Urlopy</a>
-                        </div>
-                    </div>
                 </div>
                 <!-- <a href="{{ route('employees.my-months') }}">Zobacz swoją pracę</a>
                 <a href="{{route('vacations.index') }}">Urlopy</a>  -->
