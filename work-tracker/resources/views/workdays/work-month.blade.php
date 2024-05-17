915a57c93774637fc45a5fec967ef2529eb00526
Klaudia Wolska <klaudia@macbook-air-klaudia.home>
2024-05-17 12:03:07 +0200

    work hours user view fix

--- work-tracker/resources/views/workdays/work-month.blade.php
+++ work-tracker/resources/views/workdays/work-month.blade.php
@@ -41,6 +41,7 @@
     }
     .box-1, .box-2, .box-3, .box-4{
         height: auto;
+        border-radius: 10px;
     }
     .link-color{
         color: #16C7AA;
@@ -56,7 +57,7 @@
         justify-content: center;
         font-weight: bold;
         height: 20px;
-        width: 100px;
+        width: 100%;
         text-align: center;
     }
 
@@ -129,56 +130,59 @@
                             </div> 
                         </div>
                         
-                        <div class="col-md-12">
-                                <div class="dashboard-container box-1 bg-1 p-3">
-                                    @foreach(range(1, $daysInMonth) as $day)
-                                            <div class="p-3">
-                                                <div class="func-box">{{ $day }}.{{ $month }}.{{ $year }}</div>
-                                                @php
-                                                    $workday = $workdays->firstWhere('date', Carbon\Carbon::createFromDate($year, $month, $day)->format('Y-m-d'));
-                                                @endphp
+                        <div class="row">
+                            @foreach(range(1, $daysInMonth) as $day)
+                                <div class="col-md-4 p-3">
+                                    <div class="bg-1 box-1">
+                                        <div class="func-box">{{ $day }}.{{ $month }}.{{ $year }}</div>
+                                        <div class="p-3">
+                                            @php
+                                                $workday = $workdays->firstWhere('date', Carbon\Carbon::createFromDate($year, $month, $day)->format('Y-m-d'));
+                                            @endphp
 
-                                                <form method="POST" action="{{ route('workdays.update') }}">
-                                                    @csrf
-                                                    <input type="hidden" name="user_id" value="{{ $user->id }}">
-                                                    <input type="hidden" name="date" value="{{ "$year-$month-$day" }}">
-                                                    <input type="hidden" name="month_number" value="{{ $month }}">
+                                            <form method="POST" action="{{ route('workdays.update') }}">
+                                                @csrf
+                                                <input type="hidden" name="user_id" value="{{ $user->id }}">
+                                                <input type="hidden" name="date" value="{{ "$year-$month-$day" }}">
+                                                <input type="hidden" name="month_number" value="{{ $month }}">
+                                                
+                                                <div class="py-1">
+                                                    <label>
+                                                        <input type="radio" name="attendance" value="1" {{ $workday && $workday->attendance == 1 ? 'checked' : '' }}>
+                                                        Obecny
+                                                    </label>
+                                                    <label>
+                                                        <input type="radio" name="attendance" value="0" {{ $workday && $workday->attendance == 0 ? 'checked' : '' }}>
+                                                        Nieobecny
+                                                    </label>
+                                                </div>
+                                                <div class="py-1">
+                                                    <label for="hours">Godziny przepracowane:</label>
+                                                    <input type="number" name="hours" id="hours" value="{{ $workday ? $workday->hours : '' }}">
+                                                </div>
+                                                <div class="py-1">
+                                                    <label for="absence_type">Typ absencji:</label>
+                                                    <select name="absence_type" id="absence_type">
+                                                        <option value="">Brak</option>    
+                                                        @foreach ($absenceTypes as $absenceType)
+                                                            <option value="{{ $absenceType->id }}" {{ $workday && $workday->id_absence_type == $absenceType->id ? 'selected' : '' }}>
+                                                                {{ $absenceType->name }}
+                                                            </option>
+                                                        @endforeach
+                                                    </select>
+                                                </div>
+                                                <div class="py-1">
+                                                    <label for="notes">Notatka:</label>
+                                                    <input type="text" name="notes" id="notes" value="{{ $workday ? $workday->notes : '' }}">
+                                                </div>
 
-                                                    <div class="py-1">
-                                                        <label>
-                                                            <input type="radio" name="attendance" value="1" {{ $workday && $workday->attendance == 1 ? 'checked' : '' }}>
-                                                            Obecny
-                                                        </label>
-                                                        <label>
-                                                            <input type="radio" name="attendance" value="0" {{ $workday && $workday->attendance == 0 ? 'checked' : '' }}>
-                                                            Nieobecny
-                                                        </label>
-                                                    </div>
-                                                    <div class="py-1">
-                                                        <label for="hours">Godziny przepracowane:</label>
-                                                        <input type="number" name="hours" id="hours" value="{{ $workday ? $workday->hours : '' }}">
-                                                    </div>
-                                                    <div class="py-1">
-                                                        <label for="absence_type">Typ absencji:</label>
-                                                        <select name="absence_type" id="absence_type">
-                                                            <option value="">Brak</option>    
-                                                            @foreach ($absenceTypes as $absenceType)
-                                                                <option value="{{ $absenceType->id }}" {{ $workday && $workday->id_absence_type == $absenceType->id ? 'selected' : '' }}>
-                                                                    {{ $absenceType->name }}
-                                                                </option>
-                                                            @endforeach
-                                                        </select>
-                                                    </div>
-                                                    <div class="py-1">
-                                                        <label for="notes">Notatka:</label>
-                                                        <input type="text" name="notes" id="notes" value="{{ $workday ? $workday->notes : '' }}">
-                                                    </div>
-
-                                                    <button class="button" type="submit">Zapisz</button>
-                                                </form>
-                                            </div>
-                                        @endforeach
-
+                                                <button class="button" type="submit">Zapisz</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>                        
             </div>
         </div>
     </div>
